594fc22031e4bbb5ab5646da25ffdfdf8786b901
olikraus <olikraus@gmail.com>
2016-12-20 22:10:09 +0100

    2.9.1

--- examples/page_buffer/SelectionList/SelectionList.ino
+++ examples/page_buffer/SelectionList/SelectionList.ino
@@ -111,10 +111,10 @@
 void setup(void) {
   
   // DOGS102 Shield (http://shieldlist.org/controlconnection/dogs102)
-  // u8x8.begin(/* menu_select_pin= */ 5, /* menu_next_pin= */ 4, /* menu_prev_pin= */ 2, /* menu_home_pin= */ 3);
+  // u8g2.begin(/* menu_select_pin= */ 5, /* menu_next_pin= */ 4, /* menu_prev_pin= */ 2, /* menu_up_pin= */ U8X8_PIN_NONE, /* menu_down_pin= */ U8X8_PIN_NONE, /* menu_home_pin= */ 3);
   
   // DOGM128 Shield (http://shieldlist.org/schmelle2/dogm128) + DOGXL160 Shield
-  // u8x8.begin(/* menu_select_pin= */ 2, /* menu_next_pin= */ 3, /* menu_prev_pin= */ 7, /* menu_home_pin= */ 8);
+  //u8g2.begin(/* menu_select_pin= */ 2, /* menu_next_pin= */ 3, /* menu_prev_pin= */ 7, /* menu_up_pin= */ U8X8_PIN_NO NE, /* menu_down_pin= */ U8X8_PIN_NONE, /* menu_home_pin= */ 8);
   
   // Arduboy
   //u8g2.begin(/*Select=*/ A0, /*Right/Next=*/ 5, /*Left/Prev=*/ 9, /*Up=*/ 8, /*Down=*/ 10, /*Home/Cancel=*/ A1); // Arduboy DevKit
@@ -135,7 +135,7 @@ const char *string_list =
   "Stratocumulus\n"
   "Stratus";
 
-uint8_t current_selection = 0;
+uint8_t current_selection = 1;
 
 
 void loop(void) {
@@ -145,11 +145,19 @@ void loop(void) {
     current_selection, 
     string_list);
 
-  u8g2.userInterfaceMessage(
-      "Selection:", 
-      u8x8_GetStringLineStart(current_selection, string_list ),
-      "",
-      " ok \n cancel ");
+  if ( current_selection == 0 ) {
+    u8g2.userInterfaceMessage(
+	"Nothing selected.", 
+	"",
+	"",
+	" ok ");
+  } else {
+    u8g2.userInterfaceMessage(
+	"Selection:", 
+	u8x8_GetStringLineStart(current_selection-1, string_list ),
+	"",
+	" ok \n cancel ");
+  }
 }
 
 
