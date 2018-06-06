6621c741ae85180baa5f5ad6def30000c0912a52
kraus <olikraus@gmail.com>
2018-06-06 23:16:37 +0200

    2.23.6

--- examples/page_buffer/StateBufferLoop/StateBufferLoop.ino
+++ examples/page_buffer/StateBufferLoop/StateBufferLoop.ino
@@ -1,10 +1,13 @@
 /*
 
-  Weather.ino
+  StateBufferLoop.ino
   
-  Show weather icons from open-iconic set.
-  Plus demo for text scrolling.
-  This example requires display height >= 64.
+  This example implements the for/next picture drawing loop as a state machine.
+  
+  Pro: Display refresh is distributed over several steps (only one page is drawn at a time,
+  which consumes lesser time than refreshing the complete screen)
+  
+  Contra: (A) Artefacts may be visible. (B) It is more difficult to switch between different screens.
 
   Universal 8bit Graphics Library (https://github.com/olikraus/u8g2/)
 
@@ -64,7 +67,7 @@
 //U8G2_NULL u8g2(U8G2_R0);	// null device, a 8x8 pixel display which does nothing
 //U8G2_SSD1306_128X64_NONAME_1_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8);
 //U8G2_SSD1306_128X64_NONAME_1_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 12, /* dc=*/ 4, /* reset=*/ 6);	// Arduboy (Production, Kickstarter Edition)
-//U8G2_SSD1306_128X64_NONAME_1_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8);
+U8G2_SSD1306_128X64_NONAME_1_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8);
 //U8G2_SSD1306_128X64_NONAME_1_3W_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* cs=*/ 10, /* reset=*/ 8);
 //U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 //U8G2_SSD1306_128X64_ALT0_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);   // same as the NONAME variant, but may solve the "every 2nd line skipped" problem
@@ -217,122 +220,11 @@
 // End of constructor list
 
 
-
-
-#define SUN	0
-#define SUN_CLOUD  1
-#define CLOUD 2
-#define RAIN 3
-#define THUNDER 4
-
-void drawWeatherSymbol(u8g2_uint_t x, u8g2_uint_t y, uint8_t symbol)
-{
-  // fonts used:
-  // u8g2_font_open_iconic_embedded_6x_t
-  // u8g2_font_open_iconic_weather_6x_t
-  // encoding values, see: https://github.com/olikraus/u8g2/wiki/fntgrpiconic
-  
-  switch(symbol)
-  {
-    case SUN:
-      u8g2.setFont(u8g2_font_open_iconic_weather_6x_t);
-      u8g2.drawGlyph(x, y, 69);	
-      break;
-    case SUN_CLOUD:
-      u8g2.setFont(u8g2_font_open_iconic_weather_6x_t);
-      u8g2.drawGlyph(x, y, 65);	
-      break;
-    case CLOUD:
-      u8g2.setFont(u8g2_font_open_iconic_weather_6x_t);
-      u8g2.drawGlyph(x, y, 64);	
-      break;
-    case RAIN:
-      u8g2.setFont(u8g2_font_open_iconic_weather_6x_t);
-      u8g2.drawGlyph(x, y, 67);	
-      break;
-    case THUNDER:
-      u8g2.setFont(u8g2_font_open_iconic_embedded_6x_t);
-      u8g2.drawGlyph(x, y, 67);
-      break;      
-  }
-}
-
-void drawWeather(uint8_t symbol, int degree)
-{
-  drawWeatherSymbol(0, 48, symbol);
-  u8g2.setFont(u8g2_font_logisoso32_tf);
-  u8g2.setCursor(48+3, 42);
-  u8g2.print(degree);
-  u8g2.print("°C");		// requires enableUTF8Print()
-}
-
-/*
-  Draw a string with specified pixel offset. 
-  The offset can be negative.
-  Limitation: The monochrome font with 8 pixel per glyph
-*/
-void drawScrollString(int16_t offset, const char *s)
-{
-  static char buf[36];	// should for screen with up to 256 pixel width 
-  size_t len;
-  size_t char_offset = 0;
-  u8g2_uint_t dx = 0;
-  size_t visible = 0;
-  len = strlen(s);
-  if ( offset < 0 )
-  {
-    char_offset = (-offset)/8;
-    dx = offset + char_offset*8;
-    if ( char_offset >= u8g2.getDisplayWidth()/8 )
-      return;
-    visible = u8g2.getDisplayWidth()/8-char_offset+1;
-    strncpy(buf, s, visible);
-    buf[visible] = '\0';
-    u8g2.setFont(u8g2_font_8x13_mf);
-    u8g2.drawStr(char_offset*8-dx, 62, buf);
-  }
-  else
-  {
-    char_offset = offset / 8;
-    if ( char_offset >= len )
-      return;	// nothing visible
-    dx = offset - char_offset*8;
-    visible = len - char_offset;
-    if ( visible > u8g2.getDisplayWidth()/8+1 )
-      visible = u8g2.getDisplayWidth()/8+1;
-    strncpy(buf, s+char_offset, visible);
-    buf[visible] = '\0';
-    u8g2.setFont(u8g2_font_8x13_mf);
-    u8g2.drawStr(-dx, 62, buf);
-  }
-  
-}
-
-void draw(const char *s, uint8_t symbol, int degree)
-{
-  int16_t offset = -(int16_t)u8g2.getDisplayWidth();
-  int16_t len = strlen(s);
-  for(;;)
-  {
-    u8g2.firstPage();
-    do {
-      drawWeather(symbol, degree);
-      drawScrollString(offset, s);
-    } while ( u8g2.nextPage() );
-    delay(20);
-    offset+=2;
-    if ( offset > len*8+1 )
-      break;
-  }
-}
-
-
 void setup(void) {
 
   /* U8g2 Project: SSD1306 Test Board */
   //pinMode(10, OUTPUT);
   //pinMode(9, OUTPUT);
-  
   //digitalWrite(10, 0);
   //digitalWrite(9, 0);		
 
@@ -353,18 +245,49 @@ void setup(void) {
   //digitalWrite(6, 0);	
 
   u8g2.begin();  
-  u8g2.enableUTF8Print();
 }
 
-void loop(void) {
+uint16_t seconds = 0;
 
-  draw("What a beautiful day!", SUN, 27);
-  draw("The sun's come out!", SUN_CLOUD, 19);
-  draw("It's raining cats and dogs.", RAIN, 8);
-  draw("That sounds like thunder.", THUNDER, 12);
-  draw("It's stopped raining", CLOUD, 15);
+
+// this task should be called as often as possible
+void high_speed_task(void) {
+  // place your commands  here, they will be executed very often
+  // as an example, calculated the seconds since startup...
+  seconds = millis() / 1000;
+}
+
+
+// this procedure contains the u8g2 draw commands to draw someting on the screen
+void draw() {
+    u8g2.setFont(u8g2_font_logisoso58_tn);
+    u8g2.setCursor(0,60);
+    u8g2.print(seconds);
+}
+
+// this is the state machine, which will replace the do - while loop
+void draw_page(void) {
+  static uint8_t is_next_page = 0;
   
+  // call to first page, if required
+  if ( is_next_page == 0 )
+  {
+    u8g2.firstPage();
+    is_next_page = 1;
+  }
   
+  // draw our screen
+  draw();
   
+  // call to next page
+  if ( u8g2.nextPage() == 0 ) {
+    is_next_page = 0;			// ensure, that first page is called
+  }  
+}
+
+// arduno main loop
+void loop(void) {
+  high_speed_task();
+  draw_page();
 }
 
